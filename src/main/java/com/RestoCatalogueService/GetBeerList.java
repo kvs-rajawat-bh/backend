8e0185b69c676f7734f82792e2bc0bf85a01a0e5
kvsrajawat <kvs.rajawat.bh@gmail.com>
2020-03-10 21:01:27 +0530

    added cross origin access to get beer list

--- src/main/java/com/RestoCatalogueService/GetBeerList.java
+++ src/main/java/com/RestoCatalogueService/GetBeerList.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins="*")
 @RestController
 public class GetBeerList {
 	
@@ -18,7 +19,7 @@ public class GetBeerList {
 	@Autowired
 	private BeerRepository beerRepo;
 	
-	@CrossOrigin(origins="http://localhost:3000")
+	
 	@GetMapping("/getBeerList")
 	public List<Beer> getBeerList(@RequestParam("breweryName") String breweryName)
 	{
